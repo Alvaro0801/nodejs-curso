6a006653ddbdc4a028fe0dd2dce4971318c03cb9
AlvaroRivera <52021118+Alvaro0801@users.noreply.github.com>
2023-02-24 16:51:48 -0500

    get y post

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ const morgan = require("morgan");
 const app=express();
 require('dotenv').config();
 app.use(morgan("dev"));
+app.use(express.json());
 app.set('PORT',process.env.PORT||3000);
+
+
+let estudiantes=[
+    {codigo:"2013-464123",nombre:"Juan"},
+    {codigo:"2018-451123",nombre:"Alejandro"},
+    {codigo:"2021-123123",nombre:"Jorge"},
+    {codigo:"2020-162333",nombre:"Richard"},
+    {codigo:"2012-123243",nombre:"Yerson"}
+];
+
 app.get('/',(req,res)=>{
     res.send("<h1>First Page</h1>");
 })
@@ -16,6 +27,26 @@ app.get('/prueba',(req,res)=>{
     res.json({nombre:"Alvaro"});
 })
 
+
+app.get('/estudiantes',(req,res)=>{
+    // res.json(estudiantes);
+    // console.log(req.query);
+
+    let codeStudent=req.query.codigo;
+    let resType=estudiantes.every(estudiante=>{
+        let exist=estudiante.codigo==codeStudent;
+        if(exist) res.send(estudiante)
+        return !exist;    })
+
+    if(resType) res.sendStatus(404);
+})
+
+app.post('/estudiantes',(req,res)=>{
+    console.log("Body: ",req.body);
+    const {codigo,nombre}=req.body;
+    estudiantes.push({codigo,nombre});
+    res.sendStatus(201);
+})
 app.get('/*',(req,res)=>{
     res.send("<h1>Page Not Found 404! </h1>")
 })
