f6f3f44ea22fc121b75f08ad7d82c8782a7baae5
gchambillaala <gchambillaala@gmail.com>
2023-02-27 16:53:20 -0500

    Actividad 1

--- src/index.js
+++ src/index.js
@@ -1,55 +1,130 @@
-const express=require("express");
+const express = require("express");
+const { compile } = require("morgan");
 const morgan = require("morgan");
-const app=express();
-require('dotenv').config();
+const app = express();
+require("dotenv").config();
 app.use(morgan("dev"));
 app.use(express.json());
-app.set('PORT',process.env.PORT||3000);
+app.set("PORT", process.env.PORT || 3000);
 
-
-let estudiantes=[
-    {codigo:"2013-464123",nombre:"Juan"},
-    {codigo:"2018-451123",nombre:"Alejandro"},
-    {codigo:"2021-123123",nombre:"Jorge"},
-    {codigo:"2020-162333",nombre:"Richard"},
-    {codigo:"2012-123243",nombre:"Yerson"}
+let estudiantes = [
+  { codigo: "2013-464123", nombre: "Juan" },
+  { codigo: "2018-451123", nombre: "Alejandro" },
+  { codigo: "2021-123123", nombre: "Jorge" },
+  { codigo: "2020-162333", nombre: "Richard" },
+  { codigo: "2012-123243", nombre: "Yerson" },
 ];
 
-app.get('/',(req,res)=>{
-    res.send("<h1>First Page</h1>");
-})
+app.get("/", (req, res) => {
+  res.send("<h1>First Page</h1>");
+});
 
-app.get('/home',(req,res)=>{
-    res.send("<h1>Home</h1>");
-})
+app.get("/home", (req, res) => {
+  res.send("<h1>Home</h1>");
+});
 
-app.get('/prueba',(req,res)=>{
-    res.json({nombre:"Alvaro"});
-})
+app.get("/prueba", (req, res) => {
+  res.json({ nombre: "Alvaro" });
+});
+
+app.get("/estudiantes", (req, res) => {
+  // console.log(req.query);
+  if (!req.query.codigo) return res.status(200).send(estudiantes);
+
+  let codeStudent = req.query.codigo;
+  let codigos = estudiantes.map((estudiante) => estudiante.codigo);
+  let i = codigos.indexOf(codeStudent);
+
+  if (i != -1) return res.send(estudiantes[i]);
+  res.sendStatus(404);
+});
+
+app.post("/estudiantes", (req, res) => {
+  console.log("Body: ", req.body);
+
+  if (!req.body.codigo || !req.body.nombre) res.sendStatus(400);
+
+  let nuevoEstudiante = {
+    codigo: req.body.codigo,
+    nombre: req.body.nombre,
+  };
+
+  let codigo = estudiantes.map((estudiante) => estudiante.codigo);
+
+  let i = codigos.indexOf(nuevoEstudiante.codigo);
+  if (i != -1) res.sendStatus(409);
+
+  estudiantes.push(nuevoEstudiante);
+  res.sendStatus(201);
+});
+
+//actividad
+
+let animals = [
+  {
+    nombre: "Perro",
+  },
+  {
+    nombre: "Oso",
+  },
+  {
+    nombre: "Gato",
+  },
+  {
+    nombre: "Caballo",
+  },
+  {
+    nombre: "Sapo",
+  },
+  {
+    nombre: "Cordero",
+  },
+  {
+    nombre: "Vaca",
+  },
+];
+
+app.get("/animals", (req, res) => {
+  if (!req.query.nombre) return res.send(animals);
+
+  let nuevoAnimal = { nombre: req.query.nombre };
+
+  let animal = animals.find(
+    ({nombre}) => nombre.toLowerCase() == nuevoAnimal.nombre.toLowerCase()
+  );
+  if (animal == undefined) return res.sendStatus(404);
+  res.send(animal);
+});
 
 
-app.get('/estudiantes',(req,res)=>{
-    // res.json(estudiantes);
-    // console.log(req.query);
+app.get("/animals/:nombre",(req,res)=>{
+    if(!req.params.nombre) return res.send(animals)
 
-    let codeStudent=req.query.codigo;
-    let resType=estudiantes.every(estudiante=>{
-        let exist=estudiante.codigo==codeStudent;
-        if(exist) res.send(estudiante)
-        return !exist;    })
+    let nuevoAnimal = { nombre: req.params.nombre };
 
-    if(resType) res.sendStatus(404);
+    let animal = animals.find(
+    ({nombre}) => nombre.toLowerCase() == nuevoAnimal.nombre.toLowerCase()
+    );
+    if (animal == undefined) return res.sendStatus(404);
+    res.send(animal);
 })
+app.post("/animals", (req, res) => {
+  if (!req.body.nombre) return res.sendStatus(422);
+  let nuevoAnimal ={
+    nombre: req.body.nombre,
+  };
 
-app.post('/estudiantes',(req,res)=>{
-    console.log("Body: ",req.body);
-    const {codigo,nombre}=req.body;
-    estudiantes.push({codigo,nombre});
-    res.sendStatus(201);
-})
-app.get('/*',(req,res)=>{
-    res.send("<h1>Page Not Found 404! </h1>")
-})
-app.listen(app.get('PORT'),()=>{
-    console.log(`Server running at port ${app.get('PORT')}`);
-})
+  let animal = animals.find(
+    ({nombre}) => nombre.toLowerCase() == nuevoAnimal.nombre.toLowerCase()
+  );
+  if (animal != undefined) return res.sendStatus(409);
+
+  animals.push(nuevoAnimal);
+  res.sendStatus(201);
+});
+app.get("/*", (req, res) => {
+  res.send("<h1>Page Not Found 404! </h1>");
+});
+app.listen(app.get("PORT"), () => {
+  console.log(`Server running at port ${app.get("PORT")}`);
+});
